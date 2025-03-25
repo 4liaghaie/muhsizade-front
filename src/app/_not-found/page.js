20beea293347286e83bff1e95a3c2564f8c05150
ali aghaie <4liaghaie@gmail.com>
2025-03-25 09:20:43 +0100

    third

---
+++ src/app/_not-found/page.js
@@ -0,0 +1,9 @@
+// app/_not-found/page.tsx
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "2rem" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you're looking for does not exist.</p>
+    </div>
+  );
+}
